bc0ef6a84820a40828b9e9b5120af7fd9f30c023
Павел Нелюбов <pavelnelubov@gmail.com>
2022-08-17 20:53:40 +0400

    Add project files.

---
+++ PasswordManager/Controllers/PasswordController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Controllers
+{
+    [ApiController]
+    [Route(nameof(PasswordController))]
+    public class PasswordController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Delete";
+        }
+    }
+}
